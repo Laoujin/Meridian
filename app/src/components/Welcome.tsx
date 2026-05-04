c666a0d17068c9fdcf857d4e5fb5dd598d110d9e
Laoujin <woutervs@hotmail.com>
2026-05-04 23:11:12 +0200

    start page for audio starting

--- app/src/components/Welcome.tsx
+++ app/src/components/Welcome.tsx
@@ -5,11 +5,10 @@ interface WelcomeProps {
 export default function Welcome({ onStart }: WelcomeProps) {
   return (
     <div className="welcome">
-      <div className="welcome__cake">🎂</div>
+      <img className="welcome__hero" src="/start1.png" alt="" />
       <h1 className="welcome__title">Happy Birthday!</h1>
       <p className="welcome__joke">
-        So you thought you were getting something nice?<br />
-        But all you got was this app?
+        You thought you were getting something nice...
       </p>
       <button className="welcome__btn" onClick={onStart}>
         Let&apos;s start
