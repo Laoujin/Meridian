b70adb7aa582da5ada85c386be7436793ac5a0b3
Laoujin <woutervs@hotmail.com>
2026-05-06 15:43:59 +0200

    Wire document.title to story.app.title

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './Root.tsx'
+import { story } from './data/story'
+
+document.title = story.app.title
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
